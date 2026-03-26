8e46f9b27578725234e8ad256dbf0434e724d4bc
Misty Singh <misty693936@gmail.com>
2026-03-26 21:13:50 +0530

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,7 @@
+const profile={
+    username:"@Misty",
+    isFollow:false,
+    followers:123,
+    following:123,
+    };
+    console.log(typeof profile["username"]);
---
+++ s5.js
@@ -0,0 +1,6 @@
+let companies =["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+companies.splice(2,1,"0la");
+//3
+companies.push("Amazon");
+//2
+companies.shift(" ");
