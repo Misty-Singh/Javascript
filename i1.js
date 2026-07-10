418bbcb089276a8099fe1d0a8c6c56030832bb73
mistykri <misty693936@gmail.com>
2026-07-10 20:43:50 +0530

    P1 Set

---
+++ i1.js
@@ -0,0 +1,24 @@
+// Create a variable of type string and try to add a number to it.
+let a = "Harry";
+let b=6
+console.log(a+b)
+// Use typeof operator to find the datatype of the string in last question.
+console.log(typeof(a+b))
+// Create a const object in javascript Can you change it to hold a number later?
+const a1 = {
+    name: "Harry",
+    section: 1,
+    isPrincipal: false
+}
+// Try to add a new key to the const object in Problem 3 were you able to do it?
+a1['friend'] = "Shubham"
+a1['name']="Rohan"
+console.log(a1)
+// Write a Js program to create a word-meaning dictionary of 5 words.
+const dict = {
+    appreciate: "recognize the full worth of.",
+    ataraxia: "a state of freedom from emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
